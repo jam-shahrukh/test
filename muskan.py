99eccd77dc9b9dfc17b8097cfe42bd6b520e4c70
jam-shahrukh <81997538+jam-shahrukh@users.noreply.github.com>
2021-04-08 10:22:44 -0700

    Update muskan.py

--- muskan.py
+++ muskan.py
@@ -128,8 +128,7 @@ def menu():
 		('python2 jam.py')
 	os.system("clear")
 	print banner
-	print ('|[✓] Name: "+name')
-	print "|[✓] ID  : "+id
+	print '  \033[1;92mLogged in user: ' + z
 	print (47*"-")
 	print "[1] Start Cloning."
 	print "[2] Clone With Pass Choice."
