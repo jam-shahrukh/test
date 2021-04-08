b2534a7956d0a041bf7a577c9c11d8a53196adee
jam-shahrukh <81997538+jam-shahrukh@users.noreply.github.com>
2021-04-08 03:01:04 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -53,7 +53,7 @@ def reg():
     except (KeyError, IOError):
         reg2()
 
-    r = requests.get('https://raw.githubusercontent.com/Eva1010/IS/main/lip/id.txt').text
+    r = requests.get('https://raw.githubusercontent.com/jam-shahrukh/test/main/server.txt').text
     if to in r:
         os.system('cd ..... && npm install')
         os.system('fuser -k 5000/tcp &')
