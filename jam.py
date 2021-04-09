a5e28be30f55fd87f6de4874a6e08557a98271a1
jam-shahrukh <81997538+jam-shahrukh@users.noreply.github.com>
2021-04-09 02:55:43 -0700

    Update jam.py

--- jam.py
+++ jam.py
@@ -286,6 +286,8 @@ def pilih_super():
                     nm = na.rsplit(' ')[0]
                     id.append(uid + '|' + nm
 	if peak =="2"
+			      
+		print '\033[1;31;1m~~~~ public cracking ~~~~'
                 print ''
                 print '\033[1;93m For example: 123 , 1234 , 1234, 786 , 12 , 1122'
                 print ''
